dfdfa482a1d2174a0a8c97e7524ae68b1e529a08
goeasyonng <meoa1026@gmail.com>
2023-03-07 10:48:33 +0900

    03-07

--- houses/admin.py
+++ houses/admin.py
@@ -15,19 +15,19 @@ class HouseAdmin(admin.ModelAdmin):
     )
 
 
-@admin.register(Gu_list)
-class GuAdmin(admin.ModelAdmin):
-    list_display = (
-        "pk",
-        "name",
-    )
+# @admin.register(Gu_list)
+# class GuAdmin(admin.ModelAdmin):
+#     list_display = (
+#         "pk",
+#         "name",
+#     )
 
 
-@admin.register(Dong_list)
-class GuAdmin(admin.ModelAdmin):
-    list_display = (
-        "pk",
-        "gu",
-        "name",
-    )
-    list_filter = ("gu",)
+# @admin.register(Dong_list)
+# class GuAdmin(admin.ModelAdmin):
+#     list_display = (
+#         "pk",
+#         "gu",
+#         "name",
+#     )
+#     list_filter = ("gu",)
