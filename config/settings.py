566cf53d36a5f3bb8ab505a7e95634e51f1bcf37
KimDuHong <kdh97048@gmail.com>
2023-03-07 18:34:45 +0900

    allowed_hosts

--- config/settings.py
+++ config/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["izuna.pythonanywhere.com"]
 
 
 # Application definition
