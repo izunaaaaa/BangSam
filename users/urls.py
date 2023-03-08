b0884d41dde28feb78cbbcc41c2859c82ce43565
BOMIN_SPACE <121272449+bomin5@users.noreply.github.com>
2023-03-08 17:01:26 +0900

    Update urls.py

--- users/urls.py
+++ users/urls.py
@@ -3,9 +3,9 @@ from . import views
 
 urlpatterns = [
     # path("", views.Users.as_view()),
-    # path("me/", views.UserMe.as_view()),
+    path("me/", views.UserMe.as_view()),
     # path("change-password", views.ChangePassword.as_view()),
     path("login/", views.LogIn.as_view()),
     # path("log-out", views.LogOut.as_view()),
-    # path("@<str:username>/", views.UserDetail.as_view()),
+    path("@<str:username>/", views.UserDetail.as_view()),
 ]
