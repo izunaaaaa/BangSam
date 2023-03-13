36d9f497731d68d89fd55152b6d63f002c640c4e
KimDuHong <kdh97048@gmail.com>
2023-03-13 21:57:54 +0900

    edit poetry.lock

--- build.sh
+++ build.sh
@@ -1,11 +1,7 @@
 #!/usr/bin/env bash
 # exit on error
 set -o errexit
-<<<<<<< HEAD
 
-=======
->>>>>>> 797b1a2f32889fdb0fe2fdada000e44be7d35275
-poetry lock --no-update
 poetry install
 
 python manage.py collectstatic --no-input
