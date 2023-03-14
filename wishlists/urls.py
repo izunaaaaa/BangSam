db8136f0a67f50a89e603c9f44fe6de8d6320edf
goeasyonng <meoa1026@gmail.com>
2023-03-14 15:01:27 +0900

    houseview

--- wishlists/urls.py
+++ wishlists/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import Wishlists
+from . import views
 
 urlpatterns = [
-    path("", Wishlists.as_view()),
+    path("", views.Wishlists.as_view()),
+    path("<int:pk>", views.WishlistDetail.as_view()),
 ]
