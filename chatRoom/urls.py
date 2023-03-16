1da5c38633284e23a509e3978258c7d445c38343
KimDuHong <kdh97048@gmail.com>
2023-03-16 23:32:10 +0900

    server 500 error solved

--- chatRoom/urls.py
+++ chatRoom/urls.py
@@ -3,6 +3,6 @@ from .views import ChattingList, ChattingRoomList, ChattingRoom
 
 urlpatterns = [
     path("", ChattingRoomList.as_view()),
-    path("<int:pk>", ChattingRoom.as_view()),
+    path("<int:pk>/", ChattingRoom.as_view()),
     path("<int:pk>/chatlist", ChattingList.as_view()),
 ]
--- config/settings.py
+++ config/settings.py
@@ -34,8 +34,9 @@ CF_ID = env("CF_ID")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = "RENDER" not in os.environ
+# DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1:3000", "127.0.0.1:8000"]
+ALLOWED_HOSTS = ["127.0.0.1"]
 
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get("RENDER_EXTERNAL_HOSTNAME")
