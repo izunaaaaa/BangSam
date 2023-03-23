725f943c52b8df546074f27a7d58a6838acad3c8
goeasyonng <meoa1026@gmail.com>
2023-03-23 15:41:59 +0900

    admin change

---
+++ options.py
@@ -0,0 +1,14 @@
+import json
+import os
+
+with open("options.json", "r", encoding="UTF-8") as json_data:
+    options = json.load(json_data)
+
+option_list = []
+for a in options:
+    option_data = {"model": "houses.Option", "fields": {"name": a["option"]}}
+    option_list.append(option_data)
+    # option_data["fields"] = {"option": a["option"]}
+
+with open("option_list.json", "w", encoding="utf-8") as m:
+    json.dump(option_list, m, ensure_ascii=False, indent=2)
