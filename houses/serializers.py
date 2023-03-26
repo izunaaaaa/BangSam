9bfa67f2dd11ae938142f997e5e5e8ff86a15577
goeasyonng <meoa1026@gmail.com>
2023-03-26 14:46:41 +0900

    03-26

--- houses/serializers.py
+++ houses/serializers.py
@@ -143,12 +143,7 @@ class HouseDetailSerializer(ModelSerializer):
     def validate(self, data):
         sell_kind = data.get("sell_kind")
         if sell_kind == "SALE":
-            if (
-                not data.get("sale")
-                or data.get("deposit")
-                or data.get("monthly_rent")
-                # or data.get("sale") < 0
-            ):
+            if not data.get("sale") or data.get("deposit") or data.get("monthly_rent"):
                 raise ValidationError("no sale or deposit / monthly_rent is exist")
 
         if sell_kind == "CHARTER":
@@ -156,7 +151,6 @@ class HouseDetailSerializer(ModelSerializer):
                 not data.get("deposit")
                 or not data.get("sale")
                 or data.get("monthly_rent")
-                # or int(data.get("deposit")) < 0
             ):
                 raise ValidationError("no deposit or no sale or monthly_rent is exist")
 
@@ -165,9 +159,8 @@ class HouseDetailSerializer(ModelSerializer):
                 not data.get("monthly_rent")
                 or not data.get("deposit")
                 or data.get("sale")
-                # or data.get("monthly_rent") < 0
             ):
-                raise ValidationError("no monthly_rent or no deposit or sale is exist")
+                raise ValidationError("no monthly_rent or no deposit or sale is exist ")
         return data
 
     def validate_room(self, data):
@@ -190,7 +183,18 @@ class HouseDetailSerializer(ModelSerializer):
             raise ValidationError("Maintenance cost data must be required")
         return data
 
-    def validate_distance_to_station(self, data):
-        if data == None or data < 0:
-            raise ValidationError("Maintenance cost data must be required")
+    def validate_sale(self, data):
+        print(self)
+        if data <= 0:
+            raise ValidationError("sale data must be greater than 0")
+        return data
+
+    def validate_deposit(self, data):
+        if data <= 0:
+            raise ValidationError("deposit data must be greater than 0")
+        return data
+
+    def validate_monthly_rent(self, data):
+        if data <= 0:
+            raise ValidationError("monthly_rent data must be greater than 0")
         return data
